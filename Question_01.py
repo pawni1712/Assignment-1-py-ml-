08db25506aafcf5220d2fa2e96a4af7ea6e753f1
pawni1712 <155287015+pawni1712@users.noreply.github.com>
2024-06-19 23:39:37 +0530

    Create Question_01.py

---
+++ Question_01.py
@@ -0,0 +1,4 @@
+#WAP to take 2 numbers as input and display their sums
+x=int(input("Enter the 1st number "))
+y=int(input("Enter the 2nd number "))
+print("The sum of ", x, " and ", y, " is ", x+y)
