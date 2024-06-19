a16eefa3cedaedf6def0c9f974ac1578f8ddb2cd
pawni1712 <155287015+pawni1712@users.noreply.github.com>
2024-06-19 23:44:52 +0530

    Create Question_09.py

---
+++ Question_09.py
@@ -0,0 +1,7 @@
+#WAP to check if a substring is present in a given string
+str="Casa de Loco"
+sub_str="Loco"
+if sub_str in str:
+  print("Yes,", sub_str, "is present in", str)
+else:
+print("No,", sub_str, "is not present in", str)
