39bf3a701a3eba42e62adac82e949737cfbf778c
pawni1712 <155287015+pawni1712@users.noreply.github.com>
2024-06-19 23:14:57 +0530

    Create Question_22.py

---
+++ Question_22.py
@@ -0,0 +1,3 @@
+#WAP that returns the minimum and maximum values in a list of numbers
+l=[435,53,21,45,564,314,32,56,23,765,876,98,234,768,674]
+print("The list of numbers is:\n", l, "\nMaximum of all:", max(l), "\nMinimum of all:", min(l))
