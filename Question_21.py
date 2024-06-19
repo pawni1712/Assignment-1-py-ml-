5121ea7ffd7cb188cc8de7dd75d0dc1128d0411f
pawni1712 <155287015+pawni1712@users.noreply.github.com>
2024-06-19 23:17:26 +0530

    Create Question_21.py

---
+++ Question_21.py
@@ -0,0 +1,3 @@
+#WAP that counts the occurance of a specific element in a list.
+l=["Spain", "India", "Portugal","Chad","Libya", "Venice", "France", "India","Mongolia", "India", "Venice", "Zimbabwe"]
+print("The list of countries is:\n", l, "\nOccurances of 'India' in the list=  ", l.count("India"))
